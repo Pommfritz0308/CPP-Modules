358ca7e27efd5b155810d64d420b1bc8845a62ca
fbohling <fbohling@student.42wolfsburg.de>
2024-02-23 17:36:11 +0100

    CPP05 ex02 main.cpp changes

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -18,31 +18,31 @@ int main (void)
 {
     Bureaucrat *b0 = createAndPrintBureaucrat("b0", 1);
     Bureaucrat *b1 = createAndPrintBureaucrat("b1", 150);
-    Bureaucrat *b2 = createAndPrintBureaucrat("b2", 50);
+    Bureaucrat *b2 = createAndPrintBureaucrat("b2", 6);
 
     AForm *f0 = new ShrubberyCreationForm("f0");
-    AForm *f1 = new RobotomyRequestForm("f1");
-    AForm *f2 = new PresidentialPardonForm("f2");
+    AForm *f1 = new PresidentialPardonForm("f1");
+    AForm *f2 = new RobotomyRequestForm("f2");
 
     try{
-        f0->beSigned(*b0);
         std::cout << *f0;
+        f0->beSigned(*b0);
     }
     catch (std::exception &e)
     {
         std::cout << e.what() << std::endl;
     }
     try{
-        f1->beSigned(*b1);
         std::cout << *f1;
+        f1->beSigned(*b1);
     }
     catch (std::exception &e)
     {
         std::cout << e.what() << std::endl;
     }
     try{
-        f2->beSigned(*b2);
         std::cout << *f2;
+        f2->beSigned(*b2);
     }
     catch (std::exception &e)
     {
@@ -52,6 +52,32 @@ int main (void)
     b0->executeForm(*f0);
     b1->executeForm(*f1);
     b2->executeForm(*f2);
+
+    try
+    {
+        f0->execute(*b0);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        f1->execute(*b2);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        f2->execute(*b1);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
     delete b0;
     delete b1;
