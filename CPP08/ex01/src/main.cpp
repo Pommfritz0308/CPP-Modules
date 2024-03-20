2f9412cbd6deaefa61354c4acc624c2a8fecbcf8
fbohling <fbohling@student.42wolfsburg.de>
2024-03-21 00:23:28 +0100

    CPP08 ex01 test

--- CPP08/ex01/src/main.cpp
+++ CPP08/ex01/src/main.cpp
@@ -2,6 +2,7 @@
 
 int main (void)
 {
+    std::cout << BOLD("TEST WITH SIZE 5\n");
     Span sp = Span(5);
 
     try
@@ -18,5 +19,36 @@ int main (void)
     {
         std::cerr << RBOLD(e.what()) << '\n';
     }
-    for (int i = 0; )
+
+    Span sp2 = sp;
+    try
+    {
+        sp2.addNumber(1);
+        std::cout << "Shortest span is: " << sp2.shortestSpan() << std::endl;
+        std::cout << "Longest span is: " << sp2.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << RBOLD(e.what()) << '\n';
+    }
+    
+    std::cout << BOLD("TEST WITH SIZE 10000\n");
+    Span big = Span(10000);
+
+    try
+    {
+        srand(time(NULL));
+        for(int i = 0; i < 10000; i++)
+        {
+            int numb = rand() % 10000;
+            big.addNumber(numb);
+        }
+        std::cout << "Shortest span is: " << big.shortestSpan() << std::endl;
+        std::cout << "Longest span is: " << big.longestSpan() << std::endl;
+
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << RBOLD(e.what()) << '\n';
+    }
 }
